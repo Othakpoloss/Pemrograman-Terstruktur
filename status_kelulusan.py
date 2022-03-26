feddfd7531df501a20ad0c0e5346338d5db6b602
Othakpoloss <99287177+Othakpoloss@users.noreply.github.com>
2022-03-26 22:51:04 +0700

    UTS
    UTS

---
+++ status_kelulusan.py
@@ -0,0 +1,56 @@
+from ast import If
+print("by bayu djauhari")
+
+BhsIndo = int(input("Masukan Nilai Bahasa Indonesia :"))
+BhsIndo = int(input("Masukan Nilai IPA :"))
+BhsIndo = int(input("Masukan Nilai Matematika :"))
+
+if BhsIndo > 100:
+    print("Maaf Ada yang tidak valid")
+elif Matematika > 100
+    print("Maaf Ada yang tidak valid")
+elif IPA > 100
+    print("Maaf Ada yang tidak valid")
+else
+    if BhsIndo < 0:
+        print("Maaf ada yang tidak valid")
+    elif Matematika <0:
+        print("Maaf Ada yang tidak valid")
+    elif Matematika <0:
+        print("Maaf Ada yang tidak valid")
+    elif IPA < 0:
+        print("Maaf Ada yang tidak valid")
+else
+    if BhsIndo < 60:
+        if Matematika <= 70:
+            if IPA < 60:
+                print("Status Kelulusan : Anda Tidak Lulus")
+                print("Nilai Bahasa Indonesia Kurang dari 60")
+                print("Nilai Matematika kurang dari 70")
+                print("Nilai IPA kurang dari 60")
+    elif BhsIndo < 60:
+        if Matematika <= 70:
+            print("Status Kelulusan : Anda Tidak Lulus")
+            print("Nilai Bahasa Indonesia Kurang dari 60")
+            print("Nilai Matematika kurang dari 70")
+    elif BhsIndo < 60:
+        if IPA <= 60:
+            print("Status Kelulusan : Anda Tidak Lulus")
+            print("Nilai Bahasa Indonesia Kurang dari 60")
+            print("Nilai IPA kurang dari 70")
+    elif IPA < 60:
+        if Matematika <= 70:
+            print("Status Kelulusan : Anda Tidak Lulus")
+            print("Nilai Bahasa Indonesia Kurang dari 60")
+            print("Nilai Matematika kurang dari 70")
+    elif BhsIndo < 60:
+        print("Status Kelulusan : Anda TIdak Lulus")
+        print("Nilai Anda Kurang Dari 60")
+    elif Matematika =< 70:
+        print("Status Kelulusan : Anda TIdak Lulus")
+        print("Nilai Anda Kurang Dari 60")
+    elif IPA < 60:
+        print("Status Kelulusan : Anda TIdak Lulus")
+        print("Nilai Anda Kurang Dari 60")
+else:
+    print("Status Kelulusan : Anda Lulus")
